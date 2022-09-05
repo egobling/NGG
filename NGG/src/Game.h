579914a9d4ec6faf72195ce8ba365c8d234b7764
Erik <90768963+egobling@users.noreply.github.com>
2022-09-05 08:52:20 +0200

    general funtions of the game
    I did it so when you  first start the game says "THE NGG" the i did it so you can input a number and if its to hight it says "too hight LOL" and if it too low it says "too low bro"

--- NGG/src/Game.h
+++ NGG/src/Game.h
@@ -12,5 +12,20 @@ int main();
 	int num, guess, tires = 0;
 	srand(time(0));	
 	num = rand() % 100 + 1;
+	cout << "THE NNG\n\n";
+
+
+	do
+	{
+		cout << " Guess a nuber:";
+		cin >> guess;
+		tries++;
+
+		if (guess > num)
+			cout << "too hight LOL\n\n";
+		else if (guess < num)
+			cout << "too low bro\n\n";
+	}
+
 }
 
